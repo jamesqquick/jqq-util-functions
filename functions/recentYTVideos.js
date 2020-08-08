fb811808804b819a59ea658b4d2fccd432dde85b
James Quick <james.q.quick@outlook.com>
2020-08-08 10:23:20 -0500

    including youtube id in data

--- functions/recentYTVideos.js
+++ functions/recentYTVideos.js
@@ -4,7 +4,11 @@ exports.handler = async (event) => {
         const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=10`;
         const res = await fetch(url);
         const data = await res.json();
-        const minifiedData = data.items.map((item) => item.snippet);
+        console.log(data.items[0].id);
+        const minifiedData = data.items.map((item) => ({
+            ...item.snippet,
+            videoId: item.id.videoId,
+        }));
         return {
             statusCode: 200,
             headers: {
