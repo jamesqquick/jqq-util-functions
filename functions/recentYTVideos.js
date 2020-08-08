7199f1e934d9f382098fb707748f014cc38889cb
James Quick <james.q.quick@outlook.com>
2020-08-08 09:07:25 -0500

    Typo and bug

--- functions/recentYTVideos.js
+++ functions/recentYTVideos.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 exports.handler = async (event) => {
     try {
-        const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API_KEY}&channelId=${YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=10`;
+        const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=10`;
         const res = await fetch(url);
         const data = await res.json();
         return {
