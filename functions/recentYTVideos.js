a75e8df02b54d5f50255b1eceaffedc644fdf09f
James Quick <james.q.quick@outlook.com>
2020-08-28 13:13:11 -0500

    Taking query parameter for how many yt videos to get, maxed at 20

--- functions/recentYTVideos.js
+++ functions/recentYTVideos.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch');
 exports.handler = async (event) => {
+    const count = Math.min(event.queryStringParameters.count || 10, 20);
+
     try {
-        const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=10`;
+        const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API_KEY}&channelId=${process.env.YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=${count}`;
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data.items[0].id);
         const minifiedData = data.items.map((item) => ({
             ...item.snippet,
             videoId: item.id.videoId,
