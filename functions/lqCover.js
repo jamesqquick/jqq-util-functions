f2c6197990e2cfe9f1487739724f9ed4f10bac57
James Quick <james.q.quick@outlook.com>
2020-09-12 20:13:03 -0500

    Cors

--- functions/lqCover.js
+++ functions/lqCover.js
@@ -4,6 +4,20 @@ const simpleReturn = require('netlify-functions-simple-return');
 exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     const { title, guest, guestTitle, guestImage, time } = body;
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    };
+
+    if (event.httpMethod !== 'POST') {
+        // To enable CORS
+        return {
+            statusCode: 200, // <-- Important!
+            headers,
+            body: 'This was not a POST request!',
+        };
+    }
     const url = generateLearningQuickCoverURL(
         title,
         guest,
